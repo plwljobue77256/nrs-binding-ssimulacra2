a114fecbb10055cc7503aea5bdd40a999874bca8
yitao.wyt <yitao.wyt@antgroup.com>
2024-05-30 10:44:19 +0800

    chore: options with tmpDir

--- main.js
+++ main.js
@@ -9,15 +9,16 @@ function randomName() {
 }
 
 // 将非png格式的图片通过sharp转换为png格式
-function convertToPng(img) {
+function convertToPng(img, tmpDir) {
   const { ext } = path.parse(img);
   const sharpImg = sharp(img);
+  tmpDir = tmpDir || path.join(__dirname, '/tmp');
   return sharpImg.metadata().then((metadata) => {
     // 生成一个临时图片路径
-    if (!fs.existsSync(path.join(__dirname, '/.tmp'))) {
-      fs.mkdirSync(path.join(__dirname, '/.tmp'));
+    if (!fs.existsSync(tmpDir)) {
+      fs.mkdirSync(tmpDir);
     }
-    const tmpPath = path.join(__dirname, '/.tmp/', randomName() + '.png');
+    const tmpPath = path.resolve(tmpDir, randomName() + '.png');
     if (metadata.format !== 'png') {
       return sharpImg.png().toFile(tmpPath).then(() => tmpPath);
     }
@@ -30,8 +31,9 @@ function convertToPng(img) {
   });
 }
 
-function withImg(img1, img2, callback) {
-  return Promise.all([convertToPng(img1), convertToPng(img2)]).then((res) => {
+function withImg(img1, img2, callback, opts) {
+  const { tmpDir } = opts || {};
+  return Promise.all([convertToPng(img1, tmpDir), convertToPng(img2, tmpDir)]).then((res) => {
     const img1Png = res[0];
     const img2Png = res[1];
 
@@ -49,10 +51,13 @@ function withImg(img1, img2, callback) {
   });
 }
 
-function getScore(img1, img2) {
+function getScore(img1, img2, opts) {
+  const { tmpDir } = opts || {};
   return withImg(img1, img2, (img1Png, img2Png) => {
     const score = binding.getScore(img1Png, img2Png);
     return score;
+  }, {
+    tmpDir,
   });
 }
 
